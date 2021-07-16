ec91d72be23dea90970595541e62fdbcd64d3409
bangagung <56137774+bangagung@users.noreply.github.com>
2021-07-16 22:11:18 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,40 +1,43 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-"""Userbot help command"""
+""" Userbot help command """
 
+import asyncio
 from userbot import CMD_HELP
 from userbot.events import register
 
+modules = CMD_HELP
 
-@register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
-async def help_handler(event):
-    """For .help command."""
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit(f"`{args}` is not a valid module name.")
+            await event.edit("**Module Salah !!!**")
+            await asyncio.sleep(0)
+            await event.delete()
     else:
-        head = "Please specify which module do you want help for !!"
-        head2 = f"Loaded Modules : {len(CMD_HELP)}"
-        head3 = "Usage: `.help` `<module name>`"
-        head4 = "List for all available command below: "
+        await event.edit(f"**╭━━━━━━━━━━━━━━━━━━━━━╮**\
+            \n│   Help for [🔥XBOT-REMIX🔥]\
+            \n╰━━━━━━━━━━━━━━━━━━━━━╯ \
+            \n╭━━━━━━━━━━━━━━━━━━━━━╮\
+            \n│   Untuk melihat lengkap Command\
+            \n│   Contoh: .help <nama module>\
+            \n│   Modules Aktif: {len(modules)}\
+           \n╰━━━━━━━━━━━━━━━━━━━━━╯")
         string = ""
-        sep1 = "`•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••`"
-        sep2 = "`=======================================`"
-        for i in sorted(CMD_HELP):
+        for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`  |  "
-        await event.edit(
-            f"{head}\
-              \n{head2}\
-              \n{head3}\
-              \n{sep2}\
-              \n{head4}\
-              \n\n{string}\
-              \n{sep1}"
-        )
+            string += "`\t• "
+        h = await event.reply(f"•{string}•"
+                              "\n╾─────────────────────╼")
+        await asyncio.sleep(0)
+        await h.delete()
+        await event.delete()
